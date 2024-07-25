29757ff11f912a5c5e91e6dc0f585b5fc4b69dc5
Coste-stack <p.hajdus1122@gmail.com>
2024-07-26 00:51:49 +0200

    added a watch method to update for easier development

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,22 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'index_bundle.js',
         path: path.resolve(__dirname, './dist'),
+        clean: true,
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: {
+            directory: './src',
+        },
     },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/index.html',
-            title: 'Restaurant Page',
             filename: 'index_bundle.html',
             inject: 'head'
         })
