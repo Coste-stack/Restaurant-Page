d25a97aa31bbc2fd0baeecee8195c17ad424fefb
Coste-stack <p.hajdus1122@gmail.com>
2024-07-25 23:28:13 +0200

    added html-webpack-plugin and set it up

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,18 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    entry: './src/index.js',
+    output: {
+        filename: 'index_bundle.js',
+        path: path.resolve(__dirname, './dist'),
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            title: 'Restaurant Page',
+            filename: 'index_bundle.html',
+            inject: 'head'
+        })
+    ],
 };
