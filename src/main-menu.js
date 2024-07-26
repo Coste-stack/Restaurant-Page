d886b10cf5a200bcac5f2604d94074068aa2b706
Coste-stack <p.hajdus1122@gmail.com>
2024-07-26 21:40:48 +0200

    added a header to select menu

--- src/main-menu.js
+++ src/main-menu.js
@@ -2,5 +2,19 @@ export function createMainMenu(){
     const mainMenu = document.createElement('div');
     mainMenu.classList.add('mainMenu');
 
+    const header = document.createElement('header');
+    const nav = document.createElement('nav');
+    const tab = [['Main Courses'], ['Appetizers'], ['Desserts']];
+    for(let i = 0; i < tab.length; i++){
+        const button = document.createElement('button');
+        button.innerHTML = tab[i][0];
+
+        nav.appendChild(button);
+        header.appendChild(nav);
+    }
+
+    const main = document.createElement('main');
+
+    mainMenu.appendChild(header);
     return mainMenu;
 }
