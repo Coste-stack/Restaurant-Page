f3d63d7adae0204c9426d3780e5bb26e0baf616b
Coste-stack <p.hajdus1122@gmail.com>
2024-07-26 19:21:05 +0200

    added a footer and comments

---
+++ src/footer.js
@@ -0,0 +1,5 @@
+export function createFooter(){
+    const footer = document.createElement('footer');
+
+    return footer;
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './reset.css'
 import './style.css';
 import { createHeader } from './header';
+import { createFooter } from './footer';
 
 const body = document.querySelector('body');
 
 body.appendChild(createHeader());
+body.appendChild(createFooter());
